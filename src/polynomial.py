0ace8db2578e066fa199f5f4a7a4b9b49c136a20
Rafael G. Martins <rafael@rafaelmartins.com>
2009-01-01 20:10:03 -0200

    including the method __add__ to the object Polynomial

--- src/polynomial.py
+++ src/polynomial.py
@@ -47,10 +47,52 @@ class Polynomial(list):
     def __sub__(self, term):
         term_aux = [-x for x in term]
         return self.__add__(term_aux)
+    
+    
+    def __mul__(self, term):
+        a = self[:]
+        b = term[:]
+        
+        ind = 0
+        aux = []
+        for x in a:
+            aux.append((ind, x))
+            ind += 1
+        a = aux
+        
+        ind = 0
+        aux = []
+        for x in b:
+            aux.append((ind, x))
+            ind += 1
+        b = aux
+        
+        result = []
+        
+        for ord1, coef1 in a:
+            for ord2, coef2 in b:
+                result.append((ord1 + ord2, coef1 * coef2))
+        
+        ord_res = 0
+        
+        for x, y in result:
+            if x > ord_res:
+                ord_res = x
+        
+        resp = [0 for x in range(ord_res + 1)]
+        
+        for x in range(ord_res + 1):
+            resp[x] = 0
+            for c, d in result:
+                if c == x:
+                    resp[x] += d
+        
+        return Polynomial(resp)
+        
 
 if __name__ == '__main__':
-    a = Polynomial([1,0,1,5])
-    b = Polynomial([2,5])
+    a = Polynomial([1,2,3])
+    b = Polynomial([1,2,3])
     print a
     print b
-    print a - b
+    print a * b
