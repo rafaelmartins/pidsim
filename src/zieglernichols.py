a302ddc44da16190925b454ee8b3ae1a9fb08cea
Rafael G. Martins <rafael@rafaelmartins.com>
2009-05-24 01:50:36 -0300

    added Ziegler & Nichols PID tuning method

---
+++ src/zieglernichols.py
@@ -0,0 +1,56 @@
+#-*- encoding: utf-8 -*-
+#
+#       zieglernichols.py
+#       
+#       Copyright 2009 Rafael G. Martins <rafael@rafaelmartins.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, write to the Free Software
+#       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#       MA 02110-1301, USA.
+
+def ZieglerNichols(g, sample_time, total_time, n_method):
+    
+    t, y = n_method(g, sample_time, total_time)
+    
+    k = y[-1]
+    
+    t63 = __get_time_near(t, y, 0.632*k)
+    t28 = __get_time_near(t, y, 0.28*k)
+    tau = 1.5*(t63-t28)
+    L = 1.5*(t28-(t63/3))
+    
+    kp = (1.2*tau/L)/k
+    Ti = 2*L
+    Td = L/2
+    ki = kp/Ti
+    kd = kp*Td
+    
+    return kp, ki, kd
+    
+def __get_time_near(t, y, point):
+    tolerance_range = max(y) - min(y)
+    for i in range(len(y)):
+        tolerance = abs(y[i] - point)
+        if tolerance < tolerance_range:
+            my_t = t[i]
+            tolerance_range = tolerance
+    return my_t
+
+if __name__ == '__main__':
+    
+    from rk4 import RK4
+    from transferfunction import TransferFunction
+    
+    g = TransferFunction([1], [1, 2, 3])
+    
+    print ZieglerNichols(g, 0.01, 10, RK4)
