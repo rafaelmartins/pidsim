47c19f1a5c8e7f80bc06dda5b08b64e29246cc0a
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2010-03-17 21:34:03 -0300

    added method tuning_rule to pid_simulation

--- controlsystems/pid_simulation.py
+++ controlsystems/pid_simulation.py
@@ -17,6 +17,8 @@ __all__ = [
     'CohenCoon',
     'ChienHronesReswick0',
     'ChienHronesReswick20',
+    'get_time_near',
+    'tuning_rule',
 ]
 
 
@@ -190,3 +192,22 @@ def get_time_near(t, y, point):
     
     return my_t
 
+
+def tuning_rule(t, y):
+    
+    k = y[-1]
+    
+    y28 = 0.28*k
+    y63 = 0.632*k
+    yp = max(y)
+    
+    t28 = get_time_near(t, y, y28)
+    t63 = get_time_near(t, y, y63)
+    
+    alpha = (t63 - t28)/(y63 - y28)
+    
+    t0 = t28 - (y28 * alpha)
+    tp = t63 + ((yp - y63) * alpha)
+    
+    return [t0, t28, t63, tp], [0, y28, y63, yp]
+    
