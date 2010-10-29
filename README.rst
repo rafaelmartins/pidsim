b8355de99f2312fb5ea0c6fdb3e79a758a1909a9
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2010-10-29 17:37:49 -0200

    fixed README (moved to README.rst) and setup.py

---
+++ README.rst
@@ -0,0 +1,32 @@
+PIDSIM core package
+===================
+
+This package implements a basic toolbox for the study of the Control
+Systems and the simulation of PID controllers, using Python.
+
+To use the examples, please initialize this package, using::
+
+    >>> from pidsim.types import *
+    >>> from pidsim.discretization import *
+    >>> from pidsim.pid_simulation import *
+    >>> from pidsim.error import *
+
+If you need some help with the use, or can help with the development,
+please contact the author via email or visit our project website:
+
+http://pidsim.org/
+
+All the help is welcome! :)
+
+
+Basic installation
+------------------
+
+To install, type::
+
+    # python setup.py install
+
+To build doc (you'll need sphinx), type::
+
+    # make -C doc html
+
--- setup.py
+++ setup.py
@@ -5,13 +5,31 @@ from distutils.core import setup
 import pidsim
 
 setup(
-    name = pidsim.__name__,
+    name = 'pidsim',
     version = pidsim.__version__,
     license = pidsim.__license__,
     description = pidsim.__description__,
+    long_description = open('README.rst').read(),
     author = pidsim.__author__,
     author_email = pidsim.__email__,
     url = pidsim.__url__,
+    platforms = 'any',
     packages = ['pidsim'],
     package_dir={'pidsim': 'pidsim'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
+
